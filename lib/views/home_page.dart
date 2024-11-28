10bb20f942ffbb444a00aa79d6c7462ceb7199da
Darshit Shah <darshit.softrefine@gmail.com>
2024-11-28 19:22:48 +0530

    pagination issue solved

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -6,7 +6,6 @@ import '../bloc/user_bloc/user_bloc.dart';
 import '../bloc/user_bloc/user_events.dart';
 import '../bloc/user_bloc/user_states.dart';
 import '../model/user_model/user_model.dart';
-import '../repository/user_repository/user_repository.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -20,11 +19,13 @@ class _HomePageState extends State<HomePage> {
   ScrollController _scrollController = ScrollController();
   List<Data> users = [];
   bool isLoadMore = false;
-  int page = 1;
+  bool hasMore = true;
+  int page = 0;
 
   @override
   void initState() {
     _scrollController.addListener(_loadMore);
+    fetchData();
     super.initState();
   }
 
@@ -54,7 +55,6 @@ class _HomePageState extends State<HomePage> {
 
   void fetchMoreUsers() {
     final userListBloc = context.read<UserBloc>();
-    userListBloc.userRepository.getUsers(page);
     userListBloc.add(UserSubmittedEvent(pageNumber: page));
   }
 
@@ -64,62 +64,67 @@ class _HomePageState extends State<HomePage> {
           appBar: AppBar(
             title: const Text("Bloc Demo"),
           ),
-          body: BlocProvider(create: (context) => UserBloc(userRepository: UserRepository())..add(UserSubmittedEvent(pageNumber: page)),
-            child: BlocBuilder<UserBloc, UserStates>(
-                builder: (context, state){
-                  if(state is UserSuccessState){
-                    //users.clear();
-                      users.addAll(state.users);
-
-                    //print("Users ${state.users[0].firstName}");
-                    print("Page number $page");
-                    return ListView.builder(
-                      physics: const BouncingScrollPhysics(),
-                      controller: _scrollController,
-                      itemCount: users.length + (isLoadMore ? 1 : 0),
-                      itemBuilder: (context, index) {
-                        var userData = users[index];
-                        if(index < users.length){
-                          return Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: SizedBox(
-                              child: Card(
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Row(
-                                      children: [
-                                        CircleAvatar(child: CachedNetworkImage(imageUrl:  userData.avatar, progressIndicatorBuilder: (context, url, downloadProgress) =>
-                                            CircularProgressIndicator(value: downloadProgress.progress),
-                                          errorWidget: (context, url, error) => const Icon(Icons.error),),),
-                                        const SizedBox(width: 20,),
-                                        Text("${userData.firstName} ${users[index].lastName}"),
-                                      ],
-                                    ),
-                                    const SizedBox(height: 10,),
-                                    Text(userData.email),
-                                    const SizedBox(height: 20,),
-                                    Text("${userData.id}"),
-                                  ],
+          body: BlocListener<UserBloc, UserStates>(
+            listener: (context, state) {
+              if(state is UserSuccessState) {
+                for (var user in state.users) {
+                  if (!users.contains(user)) {
+                    users.add(user);
+                  }
+                }
+              } else if(state is UserErrorState) {
+                hasMore = false;
+              }
+            },
+              child : BlocBuilder<UserBloc, UserStates>(
+              builder: (context, state){
+                  return Stack(
+                    children: [
+                      ListView.builder(
+                        physics: const BouncingScrollPhysics(),
+                        controller: _scrollController,
+                        itemCount: users.length + 1 ,
+                        itemBuilder: (context, index) {
+                          if(index < users.length) {
+                            var userData = users[index];
+                            return Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: SizedBox(
+                                child: Card(
+                                  child: Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                      Row(
+                                        children: [
+                                          CircleAvatar(child: CachedNetworkImage(imageUrl: userData.avatar,
+                                              progressIndicatorBuilder: (context, url, downloadProgress) => CircularProgressIndicator(value: downloadProgress.progress),
+                                              errorWidget: (context, url, error) => const Icon(Icons.error),),),
+                                          const SizedBox(width: 20,),
+                                          Text("${userData.firstName} ${userData.lastName}"),
+                                        ],
+                                      ),
+                                      const SizedBox(height: 10,),
+                                      Text(userData.email),
+                                      const SizedBox(height: 20,),
+                                      Text("${userData.id}"),
+                                    ],
+                                  ),
                                 ),
                               ),
-                            ),
-                          );} else {
-                          return  const Padding(padding: EdgeInsets.all(8.0),
-                            child: Center(child: CircularProgressIndicator(),),
-                          );
-                        }
-                      },
-                    );
-                  } else if(state is UserErrorState){
-                    return Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Center(child: Text(state.error)),
-                    );
-                  } else {
-                  return const Center(child: CircularProgressIndicator(),);
-                }}
-            ),
+                            );
+                          } else {
+                            return hasMore ? const Padding(padding: EdgeInsets.all(8.0),
+                              child: Center(child: CircularProgressIndicator()),
+                            ) : const Padding(
+                              padding: EdgeInsets.all(8.0),
+                              child: Center(child: Text('No more data to load'),),
+                            );
+                          }
+                          }
+                      ),
+                    ],
+                  );
+                })
           ),
       );
   }
